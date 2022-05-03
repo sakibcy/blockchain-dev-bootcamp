e2edf50277662042b7e5f870edf63a1cd0aac563
sakibcy <sakibal623@gmail.com>
2022-05-04 00:07:47 +0600

    smart contract testing

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: MIT
+// EIP-20: Token Standard
+
 pragma solidity >=0.5.0;
 
 contract Token {
-    string name = "My name";
+    string public name = "Sakib Token";
+    string public symbol = 'SAKT';
+    uint256 public decimals = 8;
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000 * ( 10 ** decimals);
+    }
+
 }
